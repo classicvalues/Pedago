817d76184fd7173497ac4cf5ceafdfeea84df820
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 16:10:17 -0700

    COMMIT

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,11 @@
-# Example utility functions
-def format_date(date: datetime) -> str:
-    return date.strftime("%Y-%m-%d")
+import re
+from datetime import datetime
+
+def validateEmail(email: str) -> bool:
+    """Validates an email address."""
+    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    return re.match(pattern, email) is not None
+
+def formatDate(date_str: str, format: str = "%Y-%m-%d") -> datetime:
+    """Formats a date string into a datetime object."""
+    return datetime.strptime(date_str, format)
