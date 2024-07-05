504fbd44a1bce4552fe7e485bd70d2c2367059e7
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 16:08:55 -0700

    COMMIT

--- user/student_manager.py
+++ user/student_manager.py
@@ -1,19 +1,32 @@
-from .user import StudentManager  # Importing main implementations from user.py
+from .student import Student
+from typing import List
 
-class StudentManagerWrapper:
+class StudentManager:
     def __init__(self):
-        self.student_manager = StudentManager()
+        self.students = {}
 
-    def addStudent(self, student_id: str, name: str, email: str) -> bool:
-        return self.student_manager.addStudent(student_id, name, email)
+    def addStudent(self, student_id: str, student: Student) -> bool:
+        """Adds a new student to the system."""
+        if student_id not in self.students:
+            self.students[student_id] = student
+            return True
+        return False
 
-    def removeStudent(self, student_id: str) -> bool:
-        return self.student_manager.removeStudent(student_id)
+    def updateStudentDetails(self, student_id: str, name: str = None, email: str = None) -> bool:
+        """Updates student details."""
+        if student_id in self.students:
+            student = self.students[student_id]
+            if name:
+                student.updateName(name)
+            if email:
+                student.updateEmail(email)
+            return True
+        return False
 
-    def getStudent(self, student_id: str) -> dict:
-        return self.student_manager.getStudent(student_id)
+    def getStudent(self, student_id: str) -> Student:
+        """Retrieves a student by their ID."""
+        return self.students.get(student_id, None)
 
-    def listStudents(self) -> List[dict]:
-        return self.student_manager.listStudents()
-
-
+    def listStudents(self) -> List[Student]:
+        """Lists all students."""
+        return list(self.students.values())
